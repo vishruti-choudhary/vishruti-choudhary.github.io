327287de6e9c2d89c2b8483337bec0140272848a
vishruti-choudhary <choudharyvishruti@gmail.com>
2021-10-28 04:41:42 +0530

    4th week assignment

--- module4-solution-main/script.js
+++ module4-solution-main/script.js
@@ -12,10 +12,10 @@ names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
-    if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j' ){
-        console.log("Goodbye" + names[i])
+    if(names[i].charAt(0) ==='J'|| names[i].charAt(0) ==='j' ){
+        console.log("Goodbye "+  names[i])
     }
     else{
-        console.log("Hello" + names[i])
+        console.log("Hello "+  names[i])
     }
 }
