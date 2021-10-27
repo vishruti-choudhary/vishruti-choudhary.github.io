720b78ff9e2a60f9d0ca1f21b63b06348a893e4b
vishruti-choudhary <choudharyvishruti@gmail.com>
2021-10-28 02:20:21 +0530

    4th week assignment

---
+++ module3-solution-main/module4-solution-main/script.js
@@ -0,0 +1,21 @@
+var names=new Array();
+names[0]="Vishruti";
+names[1]="John";
+names[2]="James";
+names[3]="jason";
+names[4]="paul";
+names[5]="Rahul";
+names[6]="daisy";
+names[7]="paula";
+names[8]="lucy";
+names[9]="jimmy";
+
+
+for (var i = 0; i < names.length; i++) {
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
+}
