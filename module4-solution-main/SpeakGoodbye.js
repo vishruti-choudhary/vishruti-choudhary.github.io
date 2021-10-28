f0dd2ae3f246c241b86320d005d995851ad0ea77
vishruti-choudhary <80638131+vishruti-choudhary@users.noreply.github.com>
2021-10-28 22:29:07 +0530

    Delete SpeakGoodbye.js

--- module4-solution-main/SpeakGoodbye.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var speakWord = "Good bye";
-    var byeSpeaker = function (name){
-        console.log(speakWord + " " + name);
-    }
-    window.byeSpeaker = byeSpeaker;
-    
-})(window);
