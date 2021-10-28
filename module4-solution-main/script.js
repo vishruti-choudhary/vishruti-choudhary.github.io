d2bcd849140fd5038827d2a41c1e1b3073d43a37
vishruti-choudhary <choudharyvishruti@gmail.com>
2021-10-28 20:47:53 +0530

    4th week assignment

--- module4-solution-main/script.js
+++ module4-solution-main/script.js
@@ -1,10 +1,8 @@
 (function() {
     var names=["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "jim"];
     for (var i = 0; i < names.length; i++) {
-        var firstletter = names[i].charAt(0).toLowerCase();
-        if (firstletter === "J") {
-            byeSpeaker(names[i]);
-            
+        if (names[i].charAt(0)=== "J" || names[i].charAt(0)=== "j"){
+        byeSpeaker(names[i]);
         } 
         else {
             helloSpeaker(names[i]);
